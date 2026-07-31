84d0103d07a22096b17faab92bd1da53a5dbfe91
Your Name <you@example.com>
2026-07-31 17:37:18 +0530

    add .main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
-
 terraform {
-  required_version = ">= 1.5.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=5.0.0"
+    }
+  }
 }
 
-terraform {
-  required_version = ">= 1.15.0"
+provider "azurerm" {
+  features {}
 }
