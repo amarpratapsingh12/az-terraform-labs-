970817ffcc0fc89f47bcf06bef3196d0bd1a270c
Your Name <you@example.com>
2026-08-01 12:40:20 +0530

    add azure storage account

--- main.tf
+++ main.tf
@@ -15,3 +15,11 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-terraform-demo"
   location = "Central India"
 }
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "amarstorage12345"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
