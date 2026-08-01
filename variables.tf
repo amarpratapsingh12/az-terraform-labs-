44cfba77d2b966feadc3e995b8754f974b62a356
Your Name <you@example.com>
2026-08-01 16:31:16 +0530

    add azure storage account

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+  description = "Azure Resource Group Name"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure Region"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "Azure Storage Account Name"
+  type        = string
+}
