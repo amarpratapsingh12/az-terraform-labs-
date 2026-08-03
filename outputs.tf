c502d87d75ea55fdadbf6576d143a97a8463ff42
amarpratapsingh12 <amarpratapsinghlko@gmail.com>
2026-08-03 13:37:46 +0530

    output modified

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,11 @@ output "resource_group_name" {
   description = "Name of the Azure Resource Group"
   value       = azurerm_resource_group.rg.name
 }
+
+output "resource_group_id" {
+  value = azurerm_resource_group.rg.id
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.storage.name
+}
